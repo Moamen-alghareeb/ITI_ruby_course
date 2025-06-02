740bbfde8b1b60a8bbedafd29f06fb36f6a8f75e
Moamen_Alghareeb <moamenalghareeb10@gmail.com>
2025-06-02 13:50:04 +0300

    step 8

---
+++ Store2/app/helpers/productsbin/rails_helper.rb
@@ -0,0 +1,2 @@
+module Productsbin::RailsHelper
+end
---
+++ Store2/test/controllers/productsbin/rails_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Productsbin::RailsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

2dd74ba8561736140924648c21da92399e58b7aa
Moamen_Alghareeb <moamenalghareeb10@gmail.com>
2025-06-08 15:59:04 +0300

    CRUD done

--- Store2/db/schema.rb
+++ Store2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_123409) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_02_113749) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
--- Store2/test/controllers/productsbin/rails_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Productsbin::RailsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
